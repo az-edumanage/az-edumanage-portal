37a1a923ffef357c1ed7fe5f48fc343d300f70c2
hussein <orca19876@gmail.com>
2026-06-28 13:19:51 +0300

    fix: baseUrl

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: true,
-  //apiBaseUrl: 'https://api.az-edumange.com/api/v1',
-  apiBaseUrl: 'http://localhost:18080/api/v1',
+  apiBaseUrl: 'https://api.az-edumanage.com/api/v1',
 };
